b7b1acf78e43babcd1a8f6db25023cfea9a8756e
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-09-16 23:39:47 +0200

    bg-image dianamico nelle multislide

--- src/components/components_multisection/MiniJumbo.vue
+++ src/components/components_multisection/MiniJumbo.vue
@@ -1,13 +1,36 @@
 <template>
-  <div></div>
+  <div class="container" :style="{ backgroundImage: 'url(' + SrcImg + ')' }">
+    <div class="row">
+      <div class="col">
+        <h2>{{ data.title }}</h2>
+      </div>
+      <div class="col">
+        <h2>{{ data.number }}</h2>
+      </div>
+    </div>
+    <div class="row">
+      <p>{{ data.up_title }}</p>
+      <p>{{ data.paragraph }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "MiniJumbo",
   props: ["data"],
+
+  computed: {
+    SrcImg() {
+      return require(`../../assets/img/${this.data.img_background}`);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  background-size: cover;
+  min-height: 600px;
+}
 </style>
